3515922dab71e93e58e2406c6145a31b6f338962
Lukas <Lukas.v.hulst@gmail.com>
2021-12-06 13:45:59 +0100

    counter werkt nu met tellen en kleur veranderen

--- clicker.py
+++ clicker.py
@@ -1,29 +1,63 @@
 import tkinter
+from tkinter.constants import Y
 window = tkinter.Tk()
 
 window.title("Clicker")
 window.config(bg="grey")
 window.geometry("350x200")
-count = tkinter.IntVar()
+count = 0
+
+
+def f_add():
+    global count
+    count += 1
+    counter.configure(text = count)
+    calc()
 
 #button to add to count
 add = tkinter.Button(
     window,
     text="Up",
     width=25,
-    height=1
+    height=1,
+    command= f_add
 )
 add.pack()
 add.place(anchor="center", x=175, y=50)
+
+def f_subtract():
+    global count
+    count -= 1
+    counter.configure(text = count)
+    calc()
 
 #button to lower count
 subtract = tkinter.Button(
     window,
     text="Down",
     width=25,
-    height=1
+    height=1,
+    command=f_subtract
 )
 subtract.pack()
-subtract.place(anchor="center",x=175, y = 150)
+subtract.place(anchor="center", x=175, y=150)
+
+#shows current count
+counter = tkinter.Label(
+    window,
+    height=1,
+    width=25,
+    text=count
+)
+counter.pack()
+counter.place(anchor="center", x=175, y=100)
+
+def calc():
+    if count > 0:
+        window.config(bg = "green")
+    elif count < 0 :
+        window.config(bg= "red")
+    else:
+        window.config(bg= "grey")
 
 window.mainloop()
